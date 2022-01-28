0b05978b697c2b6c2f24bfd65c08ed7cd452a285
RVladislav <rozbash@sfedu.ru>
2022-01-28 13:21:30 +0300

    Version 0.0.1 start screen

---
+++ app/src/main/java/com/example/lifestyle/StartScreen.java
@@ -0,0 +1,15 @@
+package com.example.lifestyle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class StartScreen extends AppCompatActivity {
+Button mainButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.srartscreen);
+    }
+}
