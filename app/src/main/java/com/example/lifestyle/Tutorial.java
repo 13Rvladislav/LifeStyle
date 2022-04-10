844191d28f7e011ddecaf82e05417e3e0470f727
asus <samarskiiros@gmail.com>
2022-04-10 17:46:53 +0300

    Tutorial

--- app/src/main/java/com/example/lifestyle/Tutorial.java
+++ app/src/main/java/com/example/lifestyle/Tutorial.java
@@ -1,14 +1,32 @@
 package com.example.lifestyle;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.widget.TextView;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
+import android.view.Gravity;
+import android.view.View;
 
 public class Tutorial extends AppCompatActivity {
+
+    private ViewFlipper viewFlipper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tutorial);
+        getSupportActionBar().hide();
+
+        viewFlipper = findViewById(R.id.view_flipper);
+    }
+    public void previousView(View view) {
+        viewFlipper.setInAnimation(this, android.R.anim.slide_in_left);
+        viewFlipper.setOutAnimation(this, android.R.anim.slide_out_right);
+        viewFlipper.showPrevious();
+    }
+    public void nextView(View view) {
+        viewFlipper.setInAnimation(this, R.anim.slide_in_right);
+        viewFlipper.setOutAnimation(this, R.anim.slide_out_left);
+        viewFlipper.showNext();
     }
 }
